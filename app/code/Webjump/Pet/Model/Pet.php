d38c42f4bd7fb2169d5a314f6cbf66dcf7ef04bb
Luis Felipe Martin Vicente <luis.vicente@webjump.com.br>
2020-06-16 12:27:13 -0300

    Repository Updates

--- app/code/Webjump/Pet/Model/Pet.php
+++ app/code/Webjump/Pet/Model/Pet.php
@@ -4,17 +4,69 @@ declare(strict_types=1);
 
 namespace Webjump\Pet\Model;
 
+use Webjump\Pet\Api\Data\PetInterface;
 use Webjump\Pet\Api\PetRepositoryInterface;
 
-class Pet implements PetRepositoryInterface
+class Pet implements PetInterface
 {
-    public function getById($id)
-    {
 
+    public function getId()
+    {
+        // TODO: Implement getId() method.
     }
 
-    public function save($pet)
+    public function setId(int $id)
     {
+        // TODO: Implement setId() method.
+    }
 
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function setName(string $petName)
+    {
+        // TODO: Implement setName() method.
+    }
+
+    public function getOwner()
+    {
+        // TODO: Implement getOwner() method.
+    }
+
+    public function setOwner(string $petOwner)
+    {
+        // TODO: Implement setOwner() method.
+    }
+
+    public function getCreatedAt()
+    {
+        // TODO: Implement getCreatedAt() method.
+    }
+
+    public function setCreatedAt()
+    {
+        // TODO: Implement setCreatedAt() method.
+    }
+
+    public function getOwnerTelephone()
+    {
+        // TODO: Implement getOwnerTelephone() method.
+    }
+
+    public function setOwnerTelephone(string $telephone)
+    {
+        // TODO: Implement setOwnerTelephone() method.
+    }
+
+    public function getOwnerId()
+    {
+        // TODO: Implement getOwnerId() method.
+    }
+
+    public function setOwnerId(int $ownerId)
+    {
+        // TODO: Implement setOwnerId() method.
     }
 }
